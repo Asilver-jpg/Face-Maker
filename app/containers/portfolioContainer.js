8176daaea66d46a1ad544d172825853bd3ea89aa
Asilver-jpg <asilver7493@gmail.com>
2020-06-15 14:35:28 -0400

    Locading screen

--- app/containers/portfolioContainer.js
+++ app/containers/portfolioContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ProjectCard from "../components/projectCard.js"
 import Navbar from "../components/navbar"
-
+import LoadingScreen from  "../components/loadingScreen.js"
 const URL = "http://localhost:3001"
 export default class PortfolioContainer extends React.Component {
     state = {
@@ -19,7 +19,7 @@ export default class PortfolioContainer extends React.Component {
     render() {
         const projectRender= this.state.user.projects.map((project) => {
             return <ProjectCard key={project.id} project={project}></ProjectCard>
-                })
+                 })
         
         return (
             <div>
